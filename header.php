3b6a7381de5c1a1eb219f417a16da3c1fcae36eb
Sicilia Toury <sicilia.scaletech@gmail.com>
2025-06-07 21:44:37 +0200

    mise a jour du header

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <header class="topbar">
          <a href="index.html" class="logo"><img src="images/logo cyber.png" alt="Logo_zlcyberconsulting" width="40px" height="40px"></a>
         <Nav>
-            <a href="index.html">Accueil</a>
-            <a href="services.html">Services</a>
-            <a href="apropos.html">A propos</a>
+            <a href="index.php" class="cta">Accueil</a>
+            <a href="services.php" class="cta">Services</a>
+            <a href="apropos.php" class="cta">A propos</a>
             <button class="contact">Contact</button>
         </Nav>
-    </header>
+</header>

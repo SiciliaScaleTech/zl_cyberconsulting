ae88194210e9b29642922f8c571c29c9383068c7
Sicilia Toury <sicilia.scaletech@gmail.com>
2025-06-07 21:44:08 +0200

    mise a jours du footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <footer class="footer">
-            <a href="mentions.php"><h4>Mentions légales</h4></a>
-            <a href="politiqueDC.php"><h4>Politique de confidentialité</h4></a>
+            <a href="mentionslegales.php"><h5>Mentions légales</h5></a>
+            <a href="politiqueDC.php"><h5>Politique de confidentialité</h5></a>
         <div class="contact">
-            <h4>Contact :</h4>
+            <h5>Contact :</h5>
             <p class="adresse">Adresse : 41 bis rue de la Ferté-Alais<br>91720 Maisse</p>
             <p class="tel">Tel : 06.10.47.75.07</p>
             <p class="Mail">Mail : zl.cyberconsulting.formation@gmail.com</p>
         </div>
        
-        <p class="developper">© 2025 ZLCyberConsulting.com. Tous droits réservés. </p>
-    </footer>     
+        <p class="developper">© 2025 ZL.Scaletech.com. Tous droits réservés. </p>
+</footer>     
